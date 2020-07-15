b771959463568c15648621797dbfbe49c25aa7d9
JeongHM <kjhm0607@gmail.com>
2020-07-15 20:03:33 +0900

    [feat] 응답코드 정의 #5

---
+++ utills/response_code.py
@@ -0,0 +1,54 @@
+RESPONSE_CODE = {
+    'SUCCESS': {
+        'message': '성공',
+        'result': None
+    },
+    'EMPTY_OBJECT': {
+        'message': '데이터가 없습니다',
+        'result': None
+    },
+    'INVALID_HEADER': {
+        'message': '필수 헤더 정보가 없습니다.',
+        'result': None
+    },
+    'ALREADY_EXIST_EMAIL': {
+        'message': '이미 사용중인 이메일 입니다.',
+        'result': None
+    },
+    'INVALID_LIST_UP_SCHEMA': {
+        'message': '파라미터가 정확하지 않습니다.',
+        'result': None
+    },
+    'INVALID_SIGN_UP_SCHEMA': {
+        'message': '회원 가입 포맷이 정확하지 않습니다.',
+        'result': None
+    },
+    'INVALID_SIGN_IN_SCHEMA': {
+        'message': '로그인 포맷이 정확하지 않습니다.',
+        'result': None
+    },
+    'FAIL_USER_LIST_UP': {
+        'message': '유저정보 조회에 실패 했습니다.',
+        'result': None
+    },
+    'FAIL_USER_SIGN_UP': {
+        'message': '회원가입에 실패 했습니다.',
+        'result': None
+    },
+    'FAIL_USER_SIGN_IN': {
+        'message': '로그인에 실패 했습니다.',
+        'result': None
+    },
+    'FAIL_GENERATE_TOKEN': {
+        'message': '토큰 생성에 실패 했습니다',
+        'result': None
+    },
+    'INVALID_TOKEN': {
+        'message': '토큰정보가 올바르지 않습니다.',
+        'result': None
+    },
+    'FAIL_REFRESH_TOKEN': {
+        'message': '토큰 리프레쉬에 실패 했습니다.',
+        'result': None
+    }
+}
